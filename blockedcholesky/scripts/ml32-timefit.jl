303b718881aa496e3a38476854cbd63ebeedb101
Douglas Bates <dmbates@gmail.com>
2026-05-31 12:15:04 -0500

    Remove Appendices (requested by editor)

--- blockedcholesky/scripts/ml32-timefit.jl
+++ blockedcholesky/scripts/ml32-timefit.jl
@@ -1,4 +1,4 @@
-using Arrow, DataFrames, MixedModels, PRIMA
+using Arrow, CSV, DataFrames, MixedModels, MKL
 
 function timefit(
     mc::Integer,
@@ -9,24 +9,52 @@ function timefit(
     backend::Symbol = :nlopt,
     optimizer::Symbol = :LN_BOBYQA,
     )
+    mc = Int8(mc)
+    uc = Int8(uc)
     df = ratings[(ratings.mnrtngs .≥ mc) .& (ratings.unrtngs .≥ uc), :]
-    nratings = size(df, 1)
-    nusers = length(unique(df.userId))
-    nmvie = length(unique(df.movieId))
+    nratings = Int32(size(df, 1))
+    nusers = Int32(length(unique(df.userId)))
+    nmvie = Int32(length(unique(df.movieId)))
     model = LinearMixedModel(@formula(rating ~ 1 + (1|userId) + (1|movieId)), df)
     model.optsum.initial_step = initial_step
     model.optsum.ftol_rel = ftol_rel
     model.optsum.ftol_abs = 1.0e-6
     model.optsum.backend = backend
     model.optsum.optimizer = optimizer
-    testpar = [inv(sqrt(2.)), inv(sqrt(3.))]    # non-integer parameter value for evaluation time check
-    objective(updateL!(setθ!(model, testpar)))  # evaluation to force compilation of methods
-    modelsz = Base.summarysize(model) / (2^30)  # size in GiB
-    L22sz = Base.summarysize(model.L[3]) / (2^30)
-    evtime = @elapsed objective(updateL!(setθ!(model, testpar)))  # time for one evaluation of objective
-    @info (; mc, uc, nratings, nusers, nmvie, modelsz, L22sz, evtime)
-    fittime = @elapsed fit!(model; progress=isinteractive())
-    nv = length(model.optsum.fitlog)
-    return model, (; mc, uc, nratings, nusers, nmvie, modelsz, L22sz, nv, fittime, evtime)
+    modelsz = Float32(Base.summarysize(model) / (2^30))  # size in GiB
+    L22sz = Float32(Base.summarysize(model.L[3]) / (2^30))
+    fittime = Float32(@elapsed fit!(model; progress=isinteractive()))
+    nv = Int8(length(model.optsum.fitlog))
+    mnevtm = fittime / nv
+    return (; mc, uc, nratings, nusers, nmvie, modelsz, L22sz, nv, fittime, mnevtm)
+end
+
+const ratings = DataFrame(Arrow.Table("./data/ratings.arrow"))
+
+function mktbl()
+    res =  @NamedTuple{
+        mc::Int8,
+        uc::Int8,
+        nratings::Int32,
+        nusers::Int32,
+        nmvie::Int32,
+        modelsz::Float32,
+        L22sz::Float32,
+        nv::Int8,
+        fittime::Float32,
+        mnevtm::Float32,
+    }[]
+    for uc in Int8.([20, 40, 80])
+        for mc in Int8.([1, 2, 5, 10, 15, 20, 50])
+            try
+                sizespeedrow = timefit(mc, uc, ratings)
+                @info sizespeedrow
+                push!(res, sizespeedrow)
+            catch e
+            finally
+                CSV.write("./data/sizespeed.csv", res)
+            end
+        end
+    end
 end
 
